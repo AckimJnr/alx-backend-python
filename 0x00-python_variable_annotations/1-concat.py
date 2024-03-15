0610eed4ce761794eaea137bee5a72f8a437c49c
acklongwe@gmail.com <acklongwe@gmail.com>
2024-03-15 22:05:39 +0200

    Add 1 concat function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+String manip function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concat the two strings and return
+    str1: first string
+    str2: second string
+    """
+    return str1+str2
