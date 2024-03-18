ad398c1548e3e46dc75070ac59c965def5eeec20
acklongwe@gmail.com <acklongwe@gmail.com>
2024-03-18 20:44:40 +0200

    Add module documentantion

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,6 +10,9 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    run functions sideby side
+    """
     tasks = [wait_random(max_delay) for _ in range(n)]
     results = await asyncio.gather(*tasks)
 
