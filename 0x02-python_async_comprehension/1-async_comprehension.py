28149f53d2be966e8b14e4fa783430578aeefd94
acklongwe@gmail.com <acklongwe@gmail.com>
2024-03-19 23:00:50 +0200

    Add function documentantio

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -9,4 +9,7 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
+    """
+    return a list of random numbers
+    """
     return [num async for num in async_generator()]
