2cdef3e8a941138a690d89e42781d99d1df7069e
acklongwe@gmail.com <acklongwe@gmail.com>
2024-03-19 21:53:15 +0200

    Add async generator function

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+module: 0-async_generator
+"""
+
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    async generator function
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
